9e7cc4bb09d89d148bebd8743ca67d2a5d3f492a
Bojack <695668678@qq.com>
2020-03-05 00:26:12 +0800

    react demo

---
+++ demo-01/src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+// import React from 'react'
+// const Component = React.Component
+
+export default class App extends Component{
+  render(){
+    return (
+      <div className="1">
+      Hello World!
+      </div>
+    )
+  }
+}
---
+++ demo-01/src/ShopCart.jsx
@@ -0,0 +1,44 @@
+// import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+
+
+export default class ShopCart extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+      list: [{
+        id: '1',
+        name: '苹果'
+      }]
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        {/* 1. */}
+        {/* <input type="text" value={this.inputValue} onChange={this.inputChange.bind(this)} /> <button >增加服务</button> */}
+        {/* 2. */}
+        <input type="text" value={this.state.inputValue} onChange={this.inputChange} /> <button onClick={this.pushList}>增加服务</button>
+        <ul>
+          {this.state.list.map((n, index) => <li onClick={this.deleteItem.bind(this, index)} key={n.id + n.name + 123}>{n.name}</li>)}
+        </ul>
+      </Fragment>
+    )
+  }
+  // 1.
+  // inputChange(e) {
+  //   this.setState({ inputValue: e.target.value })
+  // }
+  // 2. 
+  inputChange = (e) => (this.setState({ inputValue: e.target.value }))
+  pushList = () => (this.setState({ list: [...this.state.list, { id: this.state.list.length, name: this.state.inputValue }], inputValue: '' }))
+  deleteItem(index) {
+    let tempList = [...this.state.list]
+    tempList.splice(index, 1)
+    this.setState({
+      list: tempList
+    })
+  }
+} 
