4da845cd9f0ba392f0eaebfaff306daf6d8f8c44
Bojack <695668678@qq.com>
2020-03-25 22:52:03 +0800

    add admin&default

---
+++ demo-07-react-blog-service/app/controller/default/home.js
@@ -0,0 +1,9 @@
+"use strict";
+const Controller = require("egg").Controller;
+
+class HomeController extends Controller {
+  async index() {
+    this.ctx.body = "api";
+  }
+}
+module.exports = HomeController;
