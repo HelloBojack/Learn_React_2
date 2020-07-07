dcecb22fa3b4909eb9b4d04030babb4b4a576530
bojack <695668678@qq.com>
2020-07-07 23:19:23 +0800

    add demo-08

---
+++ demo-08-react-blog-admin/src/App.js
@@ -0,0 +1,18 @@
+// 根组件
+import React from 'react'
+import { Button } from 'antd'
+
+function App() {
+  return <div>
+
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <br />
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+
+  </div>
+}
+
+export default App
---
+++ demo-08-react-blog-admin/src/index.js
@@ -0,0 +1,8 @@
+// 入口文件
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
