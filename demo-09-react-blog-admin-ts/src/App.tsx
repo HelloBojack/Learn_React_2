36329807e7b328add6f006320e94222d7d07a7be
bojack <695668678@qq.com>
2020-09-07 15:46:53 +0800

    init

---
+++ demo-09-react-blog-admin-ts/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">按钮</Button>
+    </div>
+  );
+}
+
+export default App;
